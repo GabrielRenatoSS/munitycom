3a65b5e2c8d1363c04c82236dc3e6defaa80bc3a
Gabriel Renato Silveira <gabolinosouzasilveira@gmail.com>
2026-06-28 21:29:35 -0300

    celular

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,11 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="icon" type="image/png" href="/favicon.png">
         <title>MUNity.com</title>
+
+        <!-- Ícone para tela inicial do celular -->
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="manifest" href="/manifest.json">
+        <meta name="theme-color" content="#7C3AED">
         @viteReactRefresh
         @vite(['resources/js/app.jsx', 'resources/css/app.css'])
         @inertiaHead
